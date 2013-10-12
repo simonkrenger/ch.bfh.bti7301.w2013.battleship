4089cd293a216fb95b82fc2056e0f0f079b4056f
Christian Meyer <chrigu.meyer@gmail.com>
2013-10-12 13:17:23 +0200

    Some mouse events!

--- Battleship/src/main/java/ch/bfh/bti7301/w2013/battleship/gui/BoardView.java
+++ Battleship/src/main/java/ch/bfh/bti7301/w2013/battleship/gui/BoardView.java
@@ -1,20 +1,23 @@
 package ch.bfh.bti7301.w2013.battleship.gui;
 
+import javafx.event.EventHandler;
 import javafx.scene.Parent;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Line;
 import javafx.scene.shape.Rectangle;
 import javafx.scene.shape.Shape;
 import javafx.scene.transform.Rotate;
 import ch.bfh.bti7301.w2013.battleship.game.Board;
+import ch.bfh.bti7301.w2013.battleship.game.Board.Coordinates;
 import ch.bfh.bti7301.w2013.battleship.game.Missile;
 import ch.bfh.bti7301.w2013.battleship.game.Ship;
 
 public class BoardView extends Parent {
 	public static final int SIZE = 40;
 
-	public BoardView(Board board) {
-		int rows, columns;
+	public BoardView(final Board board) {
+		final int rows, columns;
 		rows = columns = board.getBoardSize();
 
 		getChildren().add(getWater(rows, columns));
@@ -46,11 +49,28 @@ public class BoardView extends Parent {
 		}
 
 		for (Missile missile : board.getPlacedMissiles()) {
-			MissileView mv = new MissileView(missile);
-			mv.relocate(SIZE * missile.getCoordinates().x,
-					SIZE * missile.getCoordinates().y);
-			getChildren().add(mv);
+			drawMissile(missile);
 		}
+
+		// This is for the opponent's board. This has to move somewhere else
+		// later, I think
+		setOnMouseClicked(new EventHandler<MouseEvent>() {
+			@Override
+			public void handle(MouseEvent e) {
+				int c = (int) (e.getX() / SIZE);
+				int r = (int) (e.getY() / SIZE);
+				Missile m = new Missile(new Coordinates(c, r));
+				board.placeMissile(m);
+				drawMissile(m);
+			}
+		});
+	}
+
+	private void drawMissile(Missile missile) {
+		MissileView mv = new MissileView(missile);
+		mv.relocate(SIZE * missile.getCoordinates().x,
+				SIZE * missile.getCoordinates().y);
+		getChildren().add(mv);
 	}
 
 	private Shape getWater(int rows, int columns) {
