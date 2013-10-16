69b3af3001cb4434a3eb64b4465fad8b0739aefc
corrf1 <corrf1@bfh.ch>
2013-10-16 18:16:54 +0200

    Changes to network package, added a GameState Enum to game oackage

--- Battleship/src/main/java/ch/bfh/bti7301/w2013/battleship/network/ConnectionHandler.java
+++ Battleship/src/main/java/ch/bfh/bti7301/w2013/battleship/network/ConnectionHandler.java
@@ -6,29 +6,26 @@ import java.net.*;
 import ch.bfh.bti7301.w2013.battleship.game.Missile;
 
 public class ConnectionHandler implements Runnable {
+	
+	private static ObjectInputStream input;
+	private static Connection conn;
 
-	private final Socket connection;
-	public ObjectInputStream in;
-	public ObjectOutputStream out;
-
-	ConnectionHandler(Socket socket) {
-		this.connection = socket;
-		try {
-			out = new ObjectOutputStream(connection.getOutputStream());
-			in = new ObjectInputStream(connection.getInputStream());
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-
+    
+	public ConnectionHandler(ObjectInputStream in, Connection connection) {
+		input = in;
+		conn = connection;
 		run();
 	}
+
+	
+
+
 
 	public void run() {
 
 		while (true) {
 			try {
-				Object inputObject = in.readObject();
+				Object inputObject = input.readObject();
 				receiveObject(inputObject);
 
 			} catch (IOException e) {
@@ -40,43 +37,24 @@ public class ConnectionHandler implements Runnable {
 		}
 	}
 
-	public Object placeShot(Missile missile) {
-		return sendObject(missile);
-	}
 
-	public Object sendReady(String ready) {
-		return sendObject(ready);
 
-	}
-
-	public Object sendStart(String start) {
-		return sendObject(start);
-	}
-
-	private Object sendObject(Object outgoingObject) {
-
+	public static void sendObject(ObjectOutputStream out, Object outgoingObject) {
 		try {
+			//if (Protocoll.checkOutput(outgoingObject)){
 			out.writeObject(outgoingObject);
+			
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 
+	}
+
+	public Object receiveObject(Object receivedObject) {
+		conn.receiveObjectToGame(receivedObject);
 		return false;
 
 	}
 
-	private static boolean receiveObject(Object receivedObject) {
-
-		return false;
-
-	}
-
-	private ObjectInputStream getInputStream() {
-		return in;
-	}
-
-	private ObjectOutputStream getOutputStream() {
-		return out;
-	}
 }
