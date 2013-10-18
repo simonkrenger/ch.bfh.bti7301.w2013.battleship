d21b7fd5e019040d03937269e5a707e5b04fcc11
corrf1 <corrf1@bfh.ch>
2013-10-18 09:32:37 +0200

    ...

---
+++ Battleship/src/main/java/ch/bfh/bti7301/w2013/battleship/network/Protocoll.java
@@ -0,0 +1,16 @@
+package ch.bfh.bti7301.w2013.battleship.network;
+
+public class Protocoll {
+	
+	// make all methods static no object used.
+	
+//	public static boolean checkOutput(String status, Object outputObject){
+//		if (outputObject == "ready"){}
+//		else if (outputObject == "start"){}
+//		else if (outputObject == ){}
+//		else if (){}
+//		
+//		
+//	}
+
+}
