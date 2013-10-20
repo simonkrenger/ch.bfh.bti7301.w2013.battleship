73c93d3c6bdce2f2699cf5776d408f9c12bc0085
Simon Krenger <simon@krenger.ch>
2013-10-20 16:36:30 +0200

    Make code more readable, applied formatting and fixed imports

--- Battleship/src/main/java/ch/bfh/bti7301/w2013/battleship/network/Connection.java
+++ Battleship/src/main/java/ch/bfh/bti7301/w2013/battleship/network/Connection.java
@@ -5,7 +5,6 @@ import java.net.*;
 
 import ch.bfh.bti7301.w2013.battleship.game.Game;
 import ch.bfh.bti7301.w2013.battleship.game.Missile;
-import ch.bfh.bti7301.w2013.battleship.game.players.GenericPlayer;
 import ch.bfh.bti7301.w2013.battleship.game.players.GenericPlayer.PlayerState;
 
 public class Connection extends Thread {
@@ -15,10 +14,9 @@ public class Connection extends Thread {
 	private ConnectionState connectionState;
 	private ConnectionStateListener connectionStateListener;
 	private static Connection instance;
-	
+
 	private ConnectionHandler handler;
 	private static Game game;
-
 
 	private Connection(Game game) throws IOException {
 		new ConnectionListener(this).start();
@@ -36,7 +34,7 @@ public class Connection extends Thread {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
+
 	}
 
 	public void connectOpponet(String Ip) {
@@ -48,7 +46,6 @@ public class Connection extends Thread {
 		try {
 			Socket socket = new Socket(Ip, GAMEPORT);
 			handler = new ConnectionHandler(this, socket);
-			
 
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
@@ -60,72 +57,68 @@ public class Connection extends Thread {
 		return instance;
 	}
 
-
 	public void placeShot(Missile missile) {
 		handler.sendObject(missile);
 	}
 
 	public void sendReady() {
-		if (game.getLocalPlayer().getPlayerState() != GenericPlayer.PlayerState.GAME_STARTED && game.getOpponent().getPlayerState() != GenericPlayer.PlayerState.GAME_STARTED){
-		throw new RuntimeException("Something is out of order here");
+		if (game.getLocalPlayer().getPlayerState() != PlayerState.GAME_STARTED
+				&& game.getOpponent().getPlayerState() != PlayerState.GAME_STARTED) {
+			throw new RuntimeException("Something is out of order here");
 		}
-		
-		handler.sendObject(GenericPlayer.PlayerState.READY);
-		game.getOpponent().setPlayerState(GenericPlayer.PlayerState.WAITING); 
-	}
 
+		handler.sendObject(PlayerState.READY);
+		game.getOpponent().setPlayerState(PlayerState.WAITING);
+	}
 
 	public void sendEnd(PlayerState end) {
 		handler.sendObject(end);
 		cleanUp();
 	}
 
-
 	public void sendStart() {
-		if (game.getLocalPlayer().getPlayerState() != GenericPlayer.PlayerState.GAME_STARTED && game.getOpponent().getPlayerState() != GenericPlayer.PlayerState.READY){
-		throw new RuntimeException("Something is out of order here");
+		if (game.getLocalPlayer().getPlayerState() != PlayerState.GAME_STARTED
+				&& game.getOpponent().getPlayerState() != PlayerState.READY) {
+			throw new RuntimeException("Something is out of order here");
 		}
-		
-		handler.sendObject(GenericPlayer.PlayerState.WAITING);
-		game.getOpponent().setPlayerState(GenericPlayer.PlayerState.PLAYING);
-	}
 
+		handler.sendObject(PlayerState.WAITING);
+		game.getOpponent().setPlayerState(PlayerState.PLAYING);
+	}
 
 	public static void receiveObjectToGame(Object object) {
 
-		if (object == GenericPlayer.PlayerState.READY){
-			game.getOpponent().setPlayerState(GenericPlayer.PlayerState.READY);
-			game.getLocalPlayer().setPlayerState(GenericPlayer.PlayerState.WAITING);
+		if (object == PlayerState.READY) {
+			game.getOpponent().setPlayerState(PlayerState.READY);
+			game.getLocalPlayer().setPlayerState(PlayerState.WAITING);
 		}
-		
-		else if (object == GenericPlayer.PlayerState.WAITING){
-			game.getOpponent().setPlayerState(GenericPlayer.PlayerState.WAITING);
-			game.getLocalPlayer().setPlayerState(GenericPlayer.PlayerState.PLAYING);
-		}
-		
-		else if (object == GenericPlayer.PlayerState.WAITING){
-			game.getOpponent().setPlayerState(GenericPlayer.PlayerState.WAITING);
-			game.getLocalPlayer().setPlayerState(GenericPlayer.PlayerState.PLAYING);
-		}
-		
-		else if (object == GenericPlayer.PlayerState.GAME_LOST){
-			game.getOpponent().setPlayerState(GenericPlayer.PlayerState.GAME_LOST);
-			game.getLocalPlayer().setPlayerState(GenericPlayer.PlayerState.GAME_WON);
-		}
-		
-		else if (object == GenericPlayer.PlayerState.GAME_WON){
-			game.getOpponent().setPlayerState(GenericPlayer.PlayerState.GAME_WON);
-			game.getLocalPlayer().setPlayerState(GenericPlayer.PlayerState.GAME_LOST);
-		}
-		
-		
-		else  {
-			
-			game.getLocalPlayer().placeMissile((Missile)object);
-				
-			}
-	}
 
+		else if (object == PlayerState.WAITING) {
+			game.getOpponent().setPlayerState(PlayerState.WAITING);
+			game.getLocalPlayer().setPlayerState(PlayerState.PLAYING);
+		}
+
+		else if (object == PlayerState.WAITING) {
+			game.getOpponent().setPlayerState(PlayerState.WAITING);
+			game.getLocalPlayer().setPlayerState(PlayerState.PLAYING);
+		}
+
+		else if (object == PlayerState.GAME_LOST) {
+			game.getOpponent().setPlayerState(PlayerState.GAME_LOST);
+			game.getLocalPlayer().setPlayerState(PlayerState.GAME_WON);
+		}
+
+		else if (object == PlayerState.GAME_WON) {
+			game.getOpponent().setPlayerState(PlayerState.GAME_WON);
+			game.getLocalPlayer().setPlayerState(PlayerState.GAME_LOST);
+		}
+
+		else {
+
+			game.getLocalPlayer().placeMissile((Missile) object);
+
+		}
+	}
 
 	public ConnectionState getConnectionState() {
 		return connectionState;
@@ -142,11 +135,11 @@ public class Connection extends Thread {
 			ConnectionStateListener connectionStateListener) {
 		this.connectionStateListener = connectionStateListener;
 	}
-	
-	public void setGame(Game game){
+
+	public void setGame(Game game) {
 		this.game = game;
 	}
-	
+
 	private void cleanUp() {
 		handler.cleanUp();
 		setConnectionState(ConnectionState.CLOSED);
