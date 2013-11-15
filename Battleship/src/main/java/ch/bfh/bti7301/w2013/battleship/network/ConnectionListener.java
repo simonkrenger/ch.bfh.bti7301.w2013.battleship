0991b8a39362fca64d4f5428a9a508be71e95ba7
Christian Meyer <chrigu.meyer@gmail.com>
2013-11-15 13:45:22 +0100

    Fixed Exception on connect

--- Battleship/src/main/java/ch/bfh/bti7301/w2013/battleship/network/ConnectionListener.java
+++ Battleship/src/main/java/ch/bfh/bti7301/w2013/battleship/network/ConnectionListener.java
@@ -1,4 +1,4 @@
-	package ch.bfh.bti7301.w2013.battleship.network;
+package ch.bfh.bti7301.w2013.battleship.network;
 
 import java.io.IOException;
 import java.net.ServerSocket;
@@ -13,36 +13,40 @@ public class ConnectionListener extends Thread {
 		try {
 			listener = new ServerSocket(Connection.GAMEPORT);
 			this.connection = connection;
-			connection.setConnectionState( ConnectionState.LISTENING, "the listener is set up");
+			connection.setConnectionState(ConnectionState.LISTENING,
+					"the listener is set up");
 		} catch (IOException e) {
 			e.printStackTrace();
-			connection.setConnectionState(ConnectionState.LISTENERERROR, "listener on port" + Connection.GAMEPORT + "could not be started.");
+			connection.setConnectionState(ConnectionState.LISTENERERROR,
+					"listener on port" + Connection.GAMEPORT
+							+ "could not be started.");
 		}
-		
-		
+
 	}
 
 	public void run() { // run the service
 		try {
-			while (true) {
+			while (!listener.isClosed()) {
 				connection.acceptOpponent(listener.accept());
 				listener.close();
 			}
 
 		} catch (IOException e) {
-			
+
 			e.printStackTrace();
-			connection.setConnectionState(ConnectionState.LISTENERERROR, "opponent could not be accepted");
+			connection.setConnectionState(ConnectionState.LISTENERERROR,
+					"opponent could not be accepted");
 		}
 	}
-	
+
 	public void closeListener() {
 		try {
 			listener.close();
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
-			connection.setConnectionState(ConnectionState.CONNECTIONERROR, "connection could not be closed");
+			connection.setConnectionState(ConnectionState.CONNECTIONERROR,
+					"connection could not be closed");
 		}
 	}
 
