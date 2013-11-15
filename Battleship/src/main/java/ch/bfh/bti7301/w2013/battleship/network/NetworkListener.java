60a86da5c5be0b4b86760d6c14ab298a153ce070
corrf1 <corrf1@bfh.ch>
2013-11-15 15:45:21 +0100

    Reformatted

--- Battleship/src/main/java/ch/bfh/bti7301/w2013/battleship/network/NetworkListener.java
+++ Battleship/src/main/java/ch/bfh/bti7301/w2013/battleship/network/NetworkListener.java
@@ -3,20 +3,21 @@ package ch.bfh.bti7301.w2013.battleship.network;
 import java.io.*;
 import java.net.*;
 
-public class NetworkListener extends Thread{
-	
-	public void run(){
-	
+public class NetworkListener extends Thread {
+
+	public void run() {
+
 		Socket udpListener = new Socket();
 		try {
-			ObjectInputStream in = new ObjectInputStream(udpListener.getInputStream());
-			ObjectOutputStream out = new ObjectOutputStream(udpListener.getOutputStream());
-			
-			while (true){
-			 readUdpSocket((String)in.readObject());
+			ObjectInputStream in = new ObjectInputStream(
+					udpListener.getInputStream());
+			ObjectOutputStream out = new ObjectOutputStream(
+					udpListener.getOutputStream());
+
+			while (true) {
+				readUdpSocket((String) in.readObject());
 			}
-			
-			
+
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -24,39 +25,36 @@ public class NetworkListener extends Thread{
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
+
 		try {
 			if (!udpListener.isClosed())
-			udpListener.close();
+				udpListener.close();
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
-		}
-	
-	private void readUdpSocket(String in){
-		if (in=)
-		
-	}
-	
-	private void sendUdpMessage(){
-		
-	}
-		
-		
+
 	}
 
-	
-//	
-//	
-//}
+	private void readUdpSocket(String in) {
+
+	}
+
+	private void sendUdpMessage() {
+
+	}
+
+}
+
 //
 //
-//try (
-//	    Socket echoSocket = new Socket(hostName, portNumber);
-//	    PrintWriter out =
-//	        new PrintWriter(echoSocket.getOutputStream(), true);
-//	    BufferedReader in =
-//	        new BufferedReader(
-//	            new InputStreamReader(echoSocket.getInputStream()));
+// }
+//
+//
+// try (
+// Socket echoSocket = new Socket(hostName, portNumber);
+// PrintWriter out =
+// new PrintWriter(echoSocket.getOutputStream(), true);
+// BufferedReader in =
+// new BufferedReader(
+// new InputStreamReader(echoSocket.getInputStream()));
