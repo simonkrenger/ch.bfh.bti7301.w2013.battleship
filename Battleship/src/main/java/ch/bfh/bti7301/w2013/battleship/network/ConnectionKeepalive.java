ab89586024653cbd3999fbef1b7ec842cd82d454
corrf1 <corrf1@bfh.ch>
2013-12-20 13:43:22 +0100

    Keepalive Object

--- Battleship/src/main/java/ch/bfh/bti7301/w2013/battleship/network/ConnectionKeepalive.java
+++ Battleship/src/main/java/ch/bfh/bti7301/w2013/battleship/network/ConnectionKeepalive.java
@@ -1,25 +1,27 @@
 package ch.bfh.bti7301.w2013.battleship.network;
 
+public class ConnectionKeepalive extends Thread {
 
-
-public class ConnectionKeepalive extends Thread{
-	
-	public ConnectionKeepalive(){
+	public ConnectionKeepalive() {
 		start();
 	}
 
-public void run (){
-	while(true){
-	Connection.getInstance().getHandler().sendObject("keepalive");
-	try {
-		sleep(5000);
-	
-	} catch (InterruptedException e) {
-		// TODO Auto-generated catch block
-		System.out.println("ConnectionKeepalive: exception sleep");
-		e.printStackTrace();
+	private static KeepAlive KEEP_ALIVE = new KeepAlive();
+
+	public void run() {
+		while (true) {
+			Connection.getInstance().getHandler().sendObject(KEEP_ALIVE);
+			try {
+				sleep(5000);
+
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				System.out.println("ConnectionKeepalive: exception sleep");
+				e.printStackTrace();
+			}
+		}
 	}
+
+	static class KeepAlive {
 	}
-}
-	
 }
